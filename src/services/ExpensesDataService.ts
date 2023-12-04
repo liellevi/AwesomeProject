afe5fccf8ecbbaaeb4c6271883ca66fe315de946
Liel Levi <liellevi04009@gmail.com>
2023-12-04 23:44:49 +0200

    Update ExpensesDataService.ts

--- src/services/ExpensesDataService.ts
+++ src/services/ExpensesDataService.ts
@@ -1,50 +1,44 @@
-import moment from 'moment'
-import { ExpenseData, ExpensesListData } from '../types'
+import moment from 'moment';
+import {ExpenseData, ExpensesListData} from '../types';
 
 export const sortExpensesByDateSections = (
-  rawData: ExpenseData[],
+  rawData: ExpenseData[]
 ): ExpensesListData[] => {
-  let retVal: ExpensesListData[] = []
+  let retVal: ExpensesListData[] = [];
   if (rawData && rawData.length > 0) {
-    let prevPairIsSameDay = false
+    
+   let prevPairIsSameDay = false;
 
-    const expenseDataWithDates: ExpenseData[] = rawData.map((item) => {
-      return { ...item, date: item.date }
-    })
-    expenseDataWithDates.sort((a, b) => Number(b?.date?.valueOf()) - Number(a?.date?.valueOf()))
-
+    const expenseDataWithDates: ExpenseData[]|any = rawData.map(item => {
+      return {...item, date: item.date};
+    });
     for (let i = 0; i < expenseDataWithDates.length; i++) {
-      const expense1 = expenseDataWithDates[i]
-      const expense2 = expenseDataWithDates[i + 1]
-      const currentItemId = i.toString()
-      if (
-        moment(new Date(expense1.date)).isSame(new Date(expense2?.date), 'day')
-      ) {
-        expense1.sectionTitle = prevPairIsSameDay
-          ? ''
-          : moment(expense2.date, 'YYYY-MM-DD').format('YYYY-MM-DD')
-        retVal.push({ id: currentItemId, data: expense1 })
-        prevPairIsSameDay = true
+      const expense1 = expenseDataWithDates[i];
+      const expense2 = expenseDataWithDates[i + 1];
+      const currentItemId = i.toString();
+      if (moment(moment(expense1.date, 'YYYY-MM-DD')).isSame(moment(expense2?.date,'YYYY-MM-DD'), 'day')){
+        expense1.sectionTitle = prevPairIsSameDay ? '' : moment(expense2.date,'YYYY-MM-DD').calendar();
+        retVal.push({id: currentItemId, data: expense1});
+        prevPairIsSameDay = true;
       } else {
-        expense1.sectionTitle = prevPairIsSameDay
-          ? ''
-          : moment(expense1.date, 'YYYY-MM-DD').format('YYYY-MM-DD')
-        retVal.push({ id: currentItemId, data: expense1 })
-        prevPairIsSameDay = false
+        expense1.sectionTitle = prevPairIsSameDay ? '' : moment(expense1.date,'YYYY-MM-DD').calendar();
+        retVal.push({id: currentItemId, data: expense1});
+        prevPairIsSameDay = false;
       }
     }
   }
-  return retVal
-}
+  return retVal;
+};
+
 
 export const calculateTotalAmount = (
   userExpensesData: ExpenseData[] | null,
 ): number => {
-  let totalExpensesAmount = 0
+  let totalExpensesAmount = 0;
   if (userExpensesData) {
-    userExpensesData?.forEach((expense) => {
-      totalExpensesAmount += expense.amount
-    })
+    userExpensesData?.forEach(expense => {
+      totalExpensesAmount += expense.amount;
+    });
   }
-  return totalExpensesAmount
-}
+  return totalExpensesAmount;
+};
